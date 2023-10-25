d9bcea4fe8827d92436212f35bf3129dbf3395c7
Georges034302 <georges.boughantous@gmail.com>
2023-10-25 13:06:26 +1100

    variables

---
+++ Python/variables.py
@@ -0,0 +1,26 @@
+x = 2 # value 2 assigned to the variable x
+print(x)
+
+x = 'Hello'
+print(x)
+
+x = True
+print(x)
+
+x , y, z = 4, True, 3.5 # assign 3 values to 3 variables
+print(x,y,z) # printing 3 values next to each other
+
+x = y = z = 2
+print(x,y,z) # printing 3 values next to each other
+print(str(x)+'\n'+
+      str(y)+'\n'+
+      str(y)+'\n') # In concatenated string all values must be strings
+
+print('hello, world') # anything within single quote is an ordinary character
+
+x = '3'
+y = 4
+result = int(x) + y # Convert string x to int x for arithmetic +
+print(result)
+
+
