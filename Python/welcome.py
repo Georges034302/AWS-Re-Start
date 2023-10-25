6350916cf3a0d7adb3a3d9f47207cb7082bec80c
Georges034302 <georges.boughantous@gmail.com>
2023-10-25 11:57:15 +1100

    welcome file

--- Python/welcome.py
+++ Python/welcome.py
@@ -1,1 +1,15 @@
 print('welcome')
+
+# printing a string next to integer
+print('x = ',2)
+
+# join a string with another value
+print('x = '+str(2)) # str function converts value to a string
+
+a = 3
+b = True
+f = 2.5
+
+print('type of a is: ' + str(type(a))) # The + joins 2 strings together
+print(type(b))
+print(type(f))
