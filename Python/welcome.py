c3e60ae23fabab200a9b136c53e74d3af8b317f3
Georges034302 <georges.boughantous@gmail.com>
2023-10-25 12:51:24 +1100

    welcome

--- Python/welcome.py
+++ Python/welcome.py
@@ -1,15 +1,16 @@
-print('welcome')
+print('welcome') # print() displays data to STDOUT
 
-# printing a string next to integer
+# printing a string next to integer using the comma , 
 print('x = ',2)
 
-# join a string with another value
+# join a string with another value using the +
 print('x = '+str(2)) # str function converts value to a string
 
 a = 3
 b = True
 f = 2.5
 
+#type function returns the type of a variable
 print('type of a is: ' + str(type(a))) # The + joins 2 strings together
 print(type(b))
 print(type(f))
